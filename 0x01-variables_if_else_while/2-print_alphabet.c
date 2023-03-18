0b5987c320ee184f61cf5fbdb1cab9789200340d
dammyaking <damola.akintola@gmail.com>
2023-03-18 10:20:45 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ *main - Prints the alphabetic
+ *Return: Always (Success)
+ */
+int main(void)
+{
+	char C;
+
+	for (C = 'a'; C <= 'z'; C++)
+	putchar(C);
+	putchar('\n');
+	return (0);
+}
