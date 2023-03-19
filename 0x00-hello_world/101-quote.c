e1ae164240471983851ce3304a6e81a596f9cf48
dammyaking <damola.akintola@gmail.com>
2023-03-18 17:01:26 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,16 @@
-#include <stdio.h>
+#include<stdio.h>
 #include <unistd.h>
+
 /**
- * main - Entry point
+ * main - Prints "and that piece of art is useful" - Dora Korpar, 2015-10-19"
  *
- * Return: Always 1 (Success)
+ * Return: Always 1 indicate successful.
  */
 int main(void)
 {
-	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	write(2,
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      59);
+
 	return (1);
 }
