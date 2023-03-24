99e789c0d12510e7663665778a186fe4658577b0
dammyaking <damola.akintola@gmail.com>
2023-03-24 16:28:32 -0700

    fizz buzz

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,23 +1,21 @@
 #include "main.h"
 /**
- * print_most_numbers - a function that prints numbers from 0-9
- *
- * Decription - break the loop if i == 2 or i == 4
- *
- * Return: 0-9, excluding 2 and 4, following by new line
- */
+ * print_most_numbers - a fu#include "main.h"
 
+/**
+ * print_most_numbers - prints numbers from 0 to 9 except 2 and 4
+ * Return: Always 0
+*/
 void print_most_numbers(void)
 {
 	int i;
 
-	for (i = 0; i <= 9; i++)
-
+	for (i = 0;  i < 10; i++)
 	{
-		if ((i == 2) || (i == 4))
-			continue;
-		else
-			_putchar(i + '0');
+		if (i != 2 && i != 4)
+		{
+			_putchar((n % 10) + '0');
+		}
 	}
 	_putchar('\n');
 }
