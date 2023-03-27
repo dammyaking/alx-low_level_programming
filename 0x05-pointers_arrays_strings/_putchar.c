443cccfbf74214bd3f18177877e05f78df93bcf0
dammyaking <damola.akintola@gmail.com>
2023-03-27 13:28:21 -0700

    array && pointer

---
+++ 0x05-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @str: The character to print
+ *
+ * Return: On 0 success 
+ */
+void _puts(char *str)
+{
+	return (0);
+}
