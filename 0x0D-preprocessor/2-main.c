909755041a4091add27ed86188bded2bac6c2cd6
dammyaking <damola.akintola@gmail.com>
2023-04-18 04:25:20 -0700

    0x0D-preprocessor

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	/*prints the name of the it was compiled with */
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
