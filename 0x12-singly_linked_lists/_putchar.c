48f62fbb2ab348b974346774c612570c7734faf2
dammyaking <damola.akintola@gmail.com>
2023-04-30 09:48:17 -0700

    print_list fix

---
+++ 0x12-singly_linked_lists/_putchar.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <unistd.h>
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * error return -1
+ */
+int _putchar(char c)
+{
+	return(write(1, &c, 1));
+}
