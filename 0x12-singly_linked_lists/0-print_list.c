0ec089843b1b01728f9e3e57074720c5ab7718dc
dammyaking <damola.akintola@gmail.com>
2023-04-30 09:40:21 -0700

    print list fix

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,19 +11,19 @@
 
 size_t print_list(const list_t *h)
 {
-	unsigned int size;
-
-	size = 0;
+	size_t nodes = 0;
 
 	while (h)
 	{
 		if (h->str == NULL)
 			printf("[0] (nil)\n");
+
 		else
-			printf("[%i] %s\n", h->len, h->str);
+			printf("[%d] %s\n", h->len, h->str);
+
+		nodes++;
 		h = h->next;
-		size += 1;
 	}
 
-	return (size);
+	return (nodes);
 }
